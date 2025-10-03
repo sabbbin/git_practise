0669ff7283587340e3090ef50b458c6b526b34e0
cloco-sabin-suwal <suwal.sabin@cloco.net>
2025-10-03 21:28:51 +0545

    feat format

--- test.ts
+++ test.ts
@@ -1,37 +1,22 @@
 
-<<<<<<< HEAD
 const c = 34
 const a = 123;
 
 const d = 34
 const b = 123
 
-
 function sabin() {
-
-  let a=1
+  let b = 1
 }
+function why() { }
 
 function sabinone() {
 
-  let a=1
+  let d = 1
 }
 
 function suwal() {
-
-  let a=1
-=======
-const c = 12
-
-
-function sabin() {
   let b = 12
-}
-function sabin1() {
 
-  let b = 12
-}
-function suwal() {
-  let b = 12
->>>>>>> feat/firstbranch
+  let a = 1;
 }
