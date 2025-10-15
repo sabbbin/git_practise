6f473d9181a4a114f33b3e7e65b971cd3989792a
sabbbin <suwal721063@gmail.com>
2025-10-15 20:03:21 +0545

    test

--- test.ts
+++ test.ts
@@ -1,21 +1,19 @@
-
-const cs = 34
+const cs = 35;
 const a = 123;
 
-const e = 123
+const e = 123;
 
 function sabin() {
-  let b = 1
+  let b = 1;
 }
-function why() { }
+function why() {}
 
 function sabintwo() {
-
-  let d = 1
+  let d = 1;
 }
 
 function suwal() {
-  let b = 12
+  let b = 12;
 
   let a = 1;
 }
