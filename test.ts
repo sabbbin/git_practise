9c2fd1a7a1ee8afc7eb6a533b29e9c927a82b8b2
sabbbin <suwal721063@gmail.com>
2025-12-12 21:20:31 +0545

    tes
    adf

--- test.ts
+++ test.ts
@@ -1,1 +1,32 @@
-const a = 'sabin'
+<<<<<<< HEAD
+const cs = 20;
+const a = 123;
+
+const e = 123;
+=======
+
+const cs = 34
+const a = 123;
+
+const e = 123
+>>>>>>> 6fb277c (ab adde Merge branch 'feat/firstbranch')
+
+function sabin() {
+  let b = 1;
+}
+function why() { }
+
+function sabintwo() {
+<<<<<<< HEAD
+  let d = 1;
+=======
+
+  let d = 1
+>>>>>>> 6fb277c (ab adde Merge branch 'feat/firstbranch')
+}
+
+function suwal() {
+  let b = 12;
+
+  let a = 1;
+}
